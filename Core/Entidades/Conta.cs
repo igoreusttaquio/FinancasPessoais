27e99320556fbb4677ed87bb8171b688ef5d9bf9
igor.silva <igor.eustaquio@groupsoftware.com.br>
2025-03-06 14:25:42 -0300

    Melhoria na implementacao do tipo de contas.

--- Core/Entidades/Conta.cs
+++ Core/Entidades/Conta.cs
@@ -1,4 +1,6 @@
-﻿namespace Core.Entidades;
+﻿using Core.Modelos;
+
+namespace Core.Entidades;
 
 public class Conta
 {
@@ -14,4 +16,17 @@ public class Conta
 
     // Relacionamento com as transações
     public ICollection<Transacao> Transacoes { get; set; }
+
+    public static TipoConta[] TiposContas = [
+             new TipoConta("corrente", "Conta Corrente"),
+             new TipoConta("poupanca", "Conta Poupança"),
+             new TipoConta("salario", "Conta Salário"),
+             new TipoConta("pagamento", "Conta de Pagamento (Digital)"),
+             new TipoConta("conjunta", "Conta Conjunta"),
+             new TipoConta("empresarial", "Conta Empresarial (PJ)"),
+             new TipoConta("universitaria", "Conta Universitária"),
+             new TipoConta("remunerada", "Conta Remunerada"),
+             new TipoConta("investimento", "Conta Investimento"),
+             new TipoConta("internacional", "Conta Internacional")
+        ];
 }
--- UI/Modais/ContaModal.cs
+++ UI/Modais/ContaModal.cs
@@ -21,20 +21,7 @@ public partial class ContaModal : Form
 
     private void PreencherTipoContas()
     {
-        TipoConta[] tiposContas = [
-             new TipoConta("corrente", "Conta Corrente"),
-             new TipoConta("poupanca", "Conta Poupança"),
-             new TipoConta("salario", "Conta Salário"),
-             new TipoConta("pagamento", "Conta de Pagamento (Digital)"),
-             new TipoConta("conjunta", "Conta Conjunta"),
-             new TipoConta("empresarial", "Conta Empresarial (PJ)"),
-             new TipoConta("universitaria", "Conta Universitária"),
-             new TipoConta("remunerada", "Conta Remunerada"),
-             new TipoConta("investimento", "Conta Investimento"),
-             new TipoConta("internacional", "Conta Internacional")
-        ];
-
-        ComboTipoConta.DefinirValores(tiposContas);
+        ComboTipoConta.DefinirValores(Conta.TiposContas);
     }
 
     private void botaoPrimario1_Click(object sender, EventArgs e)
