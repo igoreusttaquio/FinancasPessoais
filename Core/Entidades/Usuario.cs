3a62d788a2a74618106deda23c649a387c45c060
igor.silva <igor.eustaquio@groupsoftware.com.br>
2025-03-06 14:22:41 -0300

    Entidade usuario.

---
+++ Core/Entidades/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entidades;
+
+public class Usuario
+{
+    Guid UsuarioId { get; set; }
+    string Login { get; set; }
+    string Nome { get; set; }
+    byte[] SaltoSenha { get; set; }
+    string HashSenha { get; set; }
+}
